8a7352fcaca5615f24f6543db388b2ab964be3ab
heyar341 <kityonya@gmail.com>
2020-02-19 09:54:57 +0000

    app.blade.phpã®ã­ã´è¿½å 

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'freecodeGram') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'freeCodegram') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="https://goo.gl/oQrQYo" style="height: 25px" class="pr-3"></div>
+                    <div>Freecodegram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
