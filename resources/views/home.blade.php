b18d61fc031b0b5e41176aaa1a33abadb043c1e9
heyar341 <kityonya@gmail.com>
2020-02-19 12:04:41 +0000

    ãã­ãã£ã¼ã«ã®éå½¢ãä½æ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_ohc=LBeT5dZZAtIAX-Z9-Su&oh=bc2c99c4e25f67b893e9c583d9267cc2&oe=5EF649BA"
+            class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong>posts</div>
+                <div class="pr-5"><strong>24</strong>follower</div>
+                <div class="pr-5"><strong></strong>following</div>
             </div>
         </div>
     </div>
