0285ead2702a941fed8b46fa8f986efcf5d5e75a
heyar341 <kityonya@gmail.com>
2020-02-25 15:27:24 +0900

    prifleのmigrationファイルを編集

--- database/migrations/2020_02_25_061624_create_profiles_table.php
+++ database/migrations/2020_02_25_061624_create_profiles_table.php
@@ -15,6 +15,12 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->default('');
+            $table->text('description')->default('');
+            $table->string('url')->default('');
+            $table->timestamps();
+
             $table->timestamps();
         });
     }
