081b576fde35075dc0a0a7a9f09524c0c71cd66a
heyar341 <kityonya@gmail.com>
2020-02-25 21:54:06 +0900

    profileのviewのディレクトリ構成を変更

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,7 +10,7 @@ class ProfilesController extends Controller
     public function index($user_id)
     {
         $user = User::findOrFail($user_id);
-        return view('home', [
+        return view('profiles.index', [
             'user' => $user,
                 ]);
     }
