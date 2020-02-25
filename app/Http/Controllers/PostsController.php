b1ede1353dca05734ccd2caa9d358bd856c4749b
heyar341 <kityonya@gmail.com>
2020-02-25 22:15:19 +0900

    createのviewを作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function create($user_id){
+    public function create(){
         return view('posts.create');
     }
 }
