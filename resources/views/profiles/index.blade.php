c44626451c44af8e60f00fcd992362c3f3ce82a3
heyar341 <kityonya@gmail.com>
2020-02-26 21:42:17 +0900

    ログイン中のユーザー以外が使える機能を制限した

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,8 +10,13 @@
             <div class="col-9 pt-5">
                 <div class="d-flex justify-content-between align-items-baseline">
                     <h1>{{ $user->username }}</h1>
-                    <a href="/p/create">Add a Post</a>
+                    @can('update',$user->profile)
+                        <a href="/p/create">Add a Post</a>
+                    @endcan
                 </div>
+                @can('update',$user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+                @endcan
                 <div class="d-flex">
                     <div class="pr-5"><strong>{{ $user->posts()->count() }}</strong>posts</div>
                     <div class="pr-5"><strong>24</strong>follower</div>
@@ -27,7 +32,7 @@
         <div class="row pt-5">
             @foreach($user->posts as $post)
                 <div class="col-4 pb-4">
-                    <a href="/p/{{ $post-id }}">
+                    <a href="/p/{{ $post->id }}">
                     <img class="w-100" src="/storage/{{ $post->image }}">
                     </a>
                 </div>
