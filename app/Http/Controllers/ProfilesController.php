0f659b2e6e5aca74ef1359b065d513c833135b13
heyar341 <kityonya@gmail.com>
2020-02-26 22:34:41 +0900

    profile画像選択時に保存先をstorageに設定した

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Validator;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -31,7 +32,17 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        auth()->user()->profile()->update($data);
+
+
+        if(request('image')){
+            $imagePath = request('image')->store('profile','public');
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,0200);
+            $image->save();
+        }
+        auth()->user()->profile()->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
 
         return redirect("/profile/{$user->id}");
 
