06521a3abb33162d3d8a59c4ed3154bbef6265ad
heyar341 <kityonya@gmail.com>
2020-02-26 21:39:03 +0900

    Profileの編集機能を追加した

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
 
 class ProfilesController extends Controller
 {
@@ -13,5 +14,27 @@ class ProfilesController extends Controller
         return view('profiles.index', [
             'user' => $user,
                 ]);
+    }
+
+    public function edit($user){
+        $user = User::findOrFail($user);
+        $this->authorize('update',$user->profile);
+        return view('profiles.edit',compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+        auth()->user()->profile()->update($data);
+
+        return redirect("/profile/{$user->id}");
+
+
     }
 }
