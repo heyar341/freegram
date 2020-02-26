0109dd1e39056b5ace8877207a7b1842ee84ecad
heyar341 <kityonya@gmail.com>
2020-02-26 16:30:50 +0900

    画像をクリックで詳細ページが表示するようにaタグ追加

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -27,7 +27,9 @@
         <div class="row pt-5">
             @foreach($user->posts as $post)
                 <div class="col-4 pb-4">
+                    <a href="/p/{{ $post-id }}">
                     <img class="w-100" src="/storage/{{ $post->image }}">
+                    </a>
                 </div>
             @endforeach
         </div>
