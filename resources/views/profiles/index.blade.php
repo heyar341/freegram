dd1df55c30e483fca8a4ac7622809d6bab17f2b9
heyar341 <kityonya@gmail.com>
2020-02-26 15:25:25 +0900

    全ての投稿をforeachで表示するようにした

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,7 +8,10 @@
                      class="rounded-circle">
             </div>
             <div class="col-9 pt-5">
-                <div><h1>{{ $user->username }}</h1></div>
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <h1>{{ $user->username }}</h1>
+                    <a href="/p/create">Add a Post</a>
+                </div>
                 <div class="d-flex">
                     <div class="pr-5"><strong>153</strong>posts</div>
                     <div class="pr-5"><strong>24</strong>follower</div>
@@ -22,15 +25,11 @@
         </div>
 
         <div class="row pt-5">
-            <div class="col-4">
-                <img class="w-100" src="https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/e35/c0.111.928.928a/s150x150/85099648_2535244706754098_6509943847503270911_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RB_1bc2S1fUAX-iSPM8&oh=db65964a137393b4ed7679d9ce50c9b5&oe=5E8CE64D">
-            </div>
-            <div class="col-4">
-                <img class="w-100" src="https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/e35/c0.111.928.928a/s150x150/85099648_2535244706754098_6509943847503270911_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RB_1bc2S1fUAX-iSPM8&oh=db65964a137393b4ed7679d9ce50c9b5&oe=5E8CE64D">
-            </div>
-            <div class="col-4">
-                <img class="w-100" src="https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/e35/c0.111.928.928a/s150x150/85099648_2535244706754098_6509943847503270911_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RB_1bc2S1fUAX-iSPM8&oh=db65964a137393b4ed7679d9ce50c9b5&oe=5E8CE64D">
-            </div>
+            @foreach($user->posts as $post)
+                <div class="col-4">
+                    <img class="w-100" src="/storage/{{ $post->image }}">
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
