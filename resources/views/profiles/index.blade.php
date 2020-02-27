2ee15fea1e6a00de3072a88727642f038696ecd9
heyar341 <kityonya@gmail.com>
2020-02-27 12:32:26 +0900

    profileの画像を表示するように変更

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,8 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-3 p-5">
-                <img src="https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_ohc=LBeT5dZZAtIAX-Z9-Su&oh=bc2c99c4e25f67b893e9c583d9267cc2&oe=5EF649BA"
-                     class="rounded-circle">
+                <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100">
             </div>
             <div class="col-9 pt-5">
                 <div class="d-flex justify-content-between align-items-baseline">
