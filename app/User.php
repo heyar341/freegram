a2399219bde3c1c84e81a8964d26c558a492c313
heyar341 <kityonya@gmail.com>
2020-02-27 21:59:32 +0900

    profileとのmany_to_many追記

--- app/User.php
+++ app/User.php
@@ -55,6 +55,11 @@ class User extends Authenticatable
         return $this->hasOne('App\Profile');
     }
 
+
+    public function following()
+    {
+        return $this->belongsToMany('App\Profile');
+    }
     public function posts()
     {
         return $this->hasMany('App\Post')->orderBy('created_at','DESC');
