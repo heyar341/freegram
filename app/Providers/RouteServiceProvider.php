720a45ac868ee1af3ec469cfc0d3c30903c74b17
heyar341 <kityonya@gmail.com>
2020-02-28 12:17:05 +0900

    login時に/に飛ぶように変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+//    HOMEルートの時、/に飛ぶ
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
